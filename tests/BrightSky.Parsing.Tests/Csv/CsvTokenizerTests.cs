1cc06d5532095595fe422c31a4bd1421ef9f4b76
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2023-07-23 23:03:14 +0100

    Adding 1st test for CsvTokenizer.cs.

---
+++ tests/BrightSky.Parsing.Tests/Csv/CsvTokenizerTests.cs
@@ -0,0 +1,17 @@
+﻿using BrightSky.Parsing.Csv;
+
+namespace BrightSky.Parsing.Tests.Csv;
+
+public class CsvTokenizerTests
+{
+    [Theory]
+    [InlineData("")]
+    public void Tokenize_WithEmptyString_Should_BeEmpty(string input)
+    {
+        // Action
+        var actual = new CsvTokenizer().Tokenize(input);
+
+        // Assert
+        actual.Should().BeEmpty();
+    }
+}
--- tests/BrightSky.Parsing.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace BrightSky.Parsing.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
