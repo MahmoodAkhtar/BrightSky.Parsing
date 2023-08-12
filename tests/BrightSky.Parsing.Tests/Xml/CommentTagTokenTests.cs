dd53c13ad4eabe9bc89a05f040a34252d476c233
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2023-08-12 07:28:54 +0100

    Adding tests for CommentTagToken

---
+++ tests/BrightSky.Parsing.Tests/Xml/CommentTagTokenTests.cs
@@ -0,0 +1,29 @@
+﻿using BrightSky.Parsing.Xml;
+using Pidgin;
+
+namespace BrightSky.Parsing.Tests.Xml;
+
+public class CommentTagTokenTests
+{
+    [Theory]
+    [InlineData("<!---->", "")]
+    [InlineData("<!-- -->", " ")]
+    [InlineData("<!--x-->", "x")]
+    [InlineData("<!-- x -->", " x ")]
+    [InlineData("<!--xyz-->", "xyz")]
+    [InlineData("<!-- xyz -->", " xyz ")]
+    [InlineData("<!-- <> -->", " <> ")]
+    [InlineData("<!-- x-y -->", " x-y ")]
+    [InlineData("<!-- -- -->", " -- ")]
+    [InlineData("<!-- <!-- -->", " <!-- ")]
+    [InlineData("<!-- --> -->", " --> ")]
+    public void Parser_ShouldBe_AsExpected(string input, string expected)
+    {
+        // Action
+        var actual = CommentTagToken.Parser.ParseOrThrow(input);
+
+        // Assert
+        actual.Should().BeOfType<TagToken>();
+        actual.Value.Should().Be(expected);
+    }
+}
